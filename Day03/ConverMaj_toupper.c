44a053d139ebc926facbf62027d91e83ec9ed04c
SaiKYouBoi <125832789+SaiKYouBoi@users.noreply.github.com>
2025-09-08 03:00:28 +0100

    Day03

---
+++ Day03/ConverMaj_toupper.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+char name[50];
+
+printf("Entrer le nom:");
+fgets(name, sizeof(name), stdin);
+name[strlen(name) - 1] = '\0';
+
+
+int num = strlen(name);
+
+for (int i = 0; i < num; i++)
+{
+  name[i] = toupper(name[i]);
+}
+ printf("%s",name);
+
+}
