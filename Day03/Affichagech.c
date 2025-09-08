1c582faf244c8c339b53916f8de676a7a2cf4a93
SaiKYouBoi <125832789+SaiKYouBoi@users.noreply.github.com>
2025-09-08 01:47:01 +0100

    Add files via upload

---
+++ Day03/Affichagech.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+char name[50];
+
+
+printf("Entrer le nom:");
+fgets(name, sizeof(name), stdin);
+name[strlen(name) - 1] = '\0';
+
+printf("Le nom est: %s",name);
+
+}
